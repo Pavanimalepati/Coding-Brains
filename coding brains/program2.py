6e8876f91755d1ea113e1b08b24f9b9364a0dda0
Pavanimalepati <pavanimalepati16@gmail.com>
2024-05-01 11:22:06 +0530

    new

---
+++ coding brains/program2.py
@@ -0,0 +1,101 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/coding brains/program1.py
+6/5
+1.2
+6//5
+1
+2**3
+8
+4**4
+256
+6/5
+1.2
+8%5
+3
+True and False;
+False
+1 and 0
+0
+1 and 1
+1
+0 and 1
+0
+1 or 0
+1
+1 OR 0
+SyntaxError: invalid syntax
+True or False
+True
+not True
+False
+not False
+True
+'apple' and 'grapes'
+'grapes'
+s1='mango'
+s2='apple'
+s1>s2
+True
+
+
+s1='ram'
+s2='ramya'
+s1>s2
+False
+chr(65)
+'A'
+chr(97)
+'a'
+s='hello,lerning python is very easy'
+'h' in s
+True
+a=True
+b=1
+a==b
+True
+a=10
+b=20
+c=30 if a>b else 40
+>>> print(c)
+40
+>>> a=25
+>>> print(a)
+25
+>>> x=12
+>>> x>>2
+3
+>>> x=12
+>>> x<<2
+48
+>>> `12
+SyntaxError: invalid syntax
+>>> ~12
+-13
+>>> 4^5
+1
+>>> 4|5
+5
+>>> 8&9
+8
+>>> 10==20==30==40
+False
+>>> 10==10
+True
+>>>  10 or 20
+...  
+SyntaxError: unexpected indent
+>>> 10 or 20
+10
+>>> int(10.9)
+10
+>>> float(2)
+2.0
+>>> complex(10)
+(10+0j)
+>>> s='abcdefghij'
+>>> s[0:1000:1]
+'abcdefghij'
+>>> s[-4:1:-1]
+'gfedc'
---
+++ coding brains/program3.py
@@ -0,0 +1,3 @@
+s='abc abc abc'
+print(s.index('abc'))
+print(s.index('abc',0,9))
